0cd74459f3cd63fc260b8483da2fd002ab59c224
KRITIKA-KUMARI18 <kritikakumari5885@gmail.com>
2024-09-04 22:21:26 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,23 @@
+package Assignment_2;
+
+public class Constant {
+    final String INPUT_STRING = "Following are the Operations that can be performed on a String: ";
+    final String ANOTHER_INPUT = "If you want to perform any another operation press 'y' else press any other character" ;
+
+    final String APPEND_STRING = "If you want to append another string press y else press any other character";
+
+    final String ENTER_STRING = "Enter the String: ";
+    final String FIRST_STRING = "Enter the First String: ";
+    final String SECOND_STRING = "Enter the Second String: ";
+
+    final String ANOTHER_STRING = "Enter another String: ";
+
+    final String NUMOF_STRING = "Enter the number of String you want to append";
+
+    final String PREV_STRING = "Enter the String you want to replace: ";
+
+    final String REPLACE_STRING = "Enter the String you want to replace with";
+
+    final String CHOICE = "Enter your choice: ";
+
+}
