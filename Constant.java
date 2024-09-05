0e6a2203bbcae4aed56bf6fe0095e01d2447f2bd
KRITIKA-KUMARI18 <kritikakumari5885@gmail.com>
2024-09-05 14:34:34 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,29 @@
+/***
+ * This class is for all the string values that has been used to
+ *  display to the user in the StringOperations Class.
+
+ *  Owner name --> Kritika Kumari
+ *  Creation Date --> 05 September 2024
+ */
+package Assignment_2;
+
+public class Constant {
+    final String INPUT_STRING = "Following are the Operations that can be performed on a String: ";
+    final String ANOTHER_INPUT = "If you want to perform any another operation press 'y' else press any other character" ;
+    final String APPEND_STRING = "If you want to append another string press y else press any other character";
+    final String ENTER_STRING = "Enter the String: ";
+    final String PALINDROME = " is Palindrome";
+    final String NOT_PALINDROME = " is not Palindrome";
+    final String REVERSE_STRING = "Reverse String is: ";
+    final String SHIFT_INDEX = "Enter the number of characters to shift to the end of the String:";
+    final String START_INDEX = "Enter the start index from where characters has to be removed:";
+    final String LENGTH = "Enter the length of the String to be removed:";
+    final String OPTION = "Enter the Correct option:";
+    final String EXCEPTION = "Do not enter whitespace for more than one time in the string else it will count the whitespace as a word.";
+    final String TEXT = "Enter a String to extract its words: ";
+    final String FIRST_STRING = "Enter the First String: ";
+    final String NEXT_STRING = "Enter another String: ";
+    final String PREV_STRING = "Enter the String you want to replace: ";
+    final String REPLACE_STRING = "Enter the String you want to replace with";
+    final String CHOICE = "Enter your choice: ";
+}
